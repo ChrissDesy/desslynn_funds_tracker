19d2fa47b608c8967d3c8870f53e06ce22047df1
chriss desy <chimbadzwa.christopherdesmond@gmail.com>
2025-10-13 21:43:17 +0200

    fix: pdn url

--- frontend/controller/apiEngine.js
+++ frontend/controller/apiEngine.js
@@ -1,5 +1,6 @@
 
-let baseURL = 'http://127.0.0.1:5000/';
+// let baseURL = 'http://127.0.0.1:5000/';
+let baseURL = 'https://desslynn-funds-tracker.onrender.com/';
 
 // alerts
 export function showAlert(typ, msg, title){
